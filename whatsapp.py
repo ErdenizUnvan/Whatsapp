cbf2067fdd9dfd8452176313f85efdb15f304061
ErdenizUnvan <75572539+ErdenizUnvan@users.noreply.github.com>
2022-08-16 16:46:44 +0300

    whatsapp commit

---
+++ whatsapp.py
@@ -0,0 +1,12 @@
+
+#https://www.isc.upenn.edu/how-to/configuring-your-web-browser-allow-pop-windows#:~:text=Click%20the%20ellipsis%20icon%20(..,Pop%2Dups%20are%20now%20allowed.
+
+from datetime import datetime
+import pywhatkit
+now = datetime.now()
+hour=now.hour
+minute=now.minute
+second=now.second
+minute=minute+1
+pywhatkit.sendwhatmsg('+905071043776', 'Type your message here', hour, minute, second, True, 65)
+#In 30 Seconds WhatsApp will open and after 15 Seconds Message will be Delivered!
